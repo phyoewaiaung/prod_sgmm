f53b0cd2c2678a318a070eef4f8d735599de80eb
phyoewaiaung <phyoewai.aung@brycenmyanmar.com.mm>
2023-07-30 12:26:17 +0630

    test

--- app/Models/SgToMmItem.php
+++ app/Models/SgToMmItem.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SgToMmItem extends Model
+class SGtoMMItem extends Model
 {
     use HasFactory;
     protected $guarded = [];
