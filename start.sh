44157a91eb1b5929861da51092d3688f50313fa8
phyowaiaung <phyowaiaung.pxyo@gmail.com>
2026-01-19 17:01:13 +0700

    Fix port configuration to 8080

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Starting Laravel server on port 5000..."
-php artisan serve --host=0.0.0.0 --port=5000
+echo "Starting Laravel server on port 8080..."
+php artisan serve --host=0.0.0.0 --port=8080
